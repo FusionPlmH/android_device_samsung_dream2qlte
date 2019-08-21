63f5f0c6c44d11bf32d835108d4974d6462265c9
LeeMenHin <1249014784@qq.com>
2019-08-22 02:46:49 +0800

    Init device tree for Dream2qlte

---
+++ BoardConfig.mk
@@ -0,0 +1,81 @@
+DEVICE_TREE := device/samsung/dream2qlte
+LOCAL_PATH := device/samsung/dream2qlte
+
+
+ANDROID_COMPILE_WITH_JACK:= false
+
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := kryo
+
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8998
+TARGET_NO_BOOTLOADER := true
+
+# Image
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET := 0x02000000
+TARGET_PREBUILT_KERNEL := device/samsung/dream2qlte/kernel
+
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8998
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno540
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4478795776
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 58543898624
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_DEFAULT_LANGUAGE := zh_CN
+TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 36500
+TW_DEFAULT_BRIGHTNESS := 7000
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TARGET_UNIFIED_DEVICE := true
+ALLOW_MISSING_DEPENDENCIES := true
+TARGET_RECOVERY_FSTAB := device/samsung/dream2qlte/recovery.fstab
+TW_MTP_DEVICE := /dev/mtp_usb
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
+TW_NO_HAPTICS := true
+# system.prop
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
---
+++ omni_dream2qlte.mk
@@ -0,0 +1,17 @@
+# Release name
+PRODUCT_RELEASE_NAME := dream2qlte
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_dream2qlte
+PRODUCT_DEVICE := dream2qlte
+PRODUCT_NAME := omni_dream2qlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G9550
+PRODUCT_MANUFACTURER := samsung
+
